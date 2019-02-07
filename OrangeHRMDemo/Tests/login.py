309caf5f61b7af8585594e1c798d8a1814f6d248
Stan <misticosmisticos@gmail.com>
2019-02-07 18:57:59 +0200

    Path to driver reworked

--- OrangeHRMDemo/Tests/login.py
+++ OrangeHRMDemo/Tests/login.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 from OrangeHRMDemo.Pages import loginPage
 from OrangeHRMDemo.Pages import welcomePage
 import unittest
@@ -6,16 +7,20 @@ import unittest
 
 class LoginTest(unittest.TestCase):
 
+    @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Chrome(
-            executable_path="C:/Users/smarkov/PycharmProjects/OrangeHRM/OrangeHRMDemo/drivers")
+        cls.driver = webdriver.Chrome(executable_path="../drivers/chromedriver.exe")
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
 
+    @classmethod
     def test_login(self):
 
         self.driver.get("https://opensource-demo.orangehrmlive.com/")
         self.driver.find_element_by_xpath("//*")
+
+
+# if __name__ == "__main__": unittest.main()
 
 # https://opensource-demo.orangehrmlive.com/
 # https://www.youtube.com/watch?v=BURK7wMcCwU
